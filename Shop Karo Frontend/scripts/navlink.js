8a5e9209ed2ee015761f59ed2a7b8b2a8083d80f
prathmesh49 <prathmesh_kurane@yahoo.com>
2023-05-08 22:56:33 +0530

    front end

--- Shop Karo Frontend/scripts/navlink.js
+++ Shop Karo Frontend/scripts/navlink.js
@@ -1,3 +1,27 @@
+function logout() {
+  let sessionid = JSON.parse(sessionStorage.getItem("customer"));
+  var requestOptions = {
+    method: "POST",
+    redirect: "follow",
+  };
+  console.log(sessionid.uuid);
+
+  fetch(`http://localhost:8080/logout?key=${sessionid.uuid}`, requestOptions)
+    .then((result) => {
+      console.log(result);
+      console.log("check p1");
+      if (result.status >= 204) {
+        result.json().then((d) => {
+          console.log("check p2");
+        });
+        sessionStorage.removeItem("customer");
+        sessionStorage.removeItem("customer_details");
+        window.location.href = "login.html";
+      }
+    })
+    .catch((error) => console.log("error", error));
+}
+
 window.addEventListener("load", function () {
   let cart = document.getElementById("cart");
   cart.onclick = () => (window.location.href = "cart.html");
@@ -7,4 +31,7 @@ window.addEventListener("load", function () {
   order.onclick = () => (window.location.href = "order.html");
   let home = document.getElementById("home");
   home.onclick = () => (window.location.href = "customer.html");
+  document.getElementById("logout-btn").onclick = () => {
+    logout();
+  };
 });
