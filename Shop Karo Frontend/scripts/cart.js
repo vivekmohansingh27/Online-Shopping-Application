60d826bf8cbf7b0d719dc79a99b66d0f8ea88170
prathmesh49 <prathmesh_kurane@yahoo.com>
2023-05-09 02:15:00 +0530

    customer complete

--- Shop Karo Frontend/scripts/cart.js
+++ Shop Karo Frontend/scripts/cart.js
@@ -2,3 +2,131 @@ import navbar from "./navbar.js";
 
 let nav = document.getElementById("navbar");
 nav.innerHTML = navbar();
+
+function addProduct(val) {
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  fetch(
+    `http://localhost:8080/carts/viewProducts?cartId=${val}`,
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+      render(result);
+    })
+    .catch((error) => console.log("error", error));
+}
+
+function render(result) {
+  let sessionid = JSON.parse(sessionStorage.getItem("customer")) || null;
+  console.log(sessionid);
+  const productList = document.getElementById("productAppend");
+  productList.innerHTML = null;
+  let i = 1;
+  for (let product of result) {
+    //console.log(product)
+
+    const row = document.createElement("productAppend");
+
+    row.innerHTML = `
+    <div class="card mb-3" style="max-width: 540px">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="${product.image}" class="img-fluid rounded-start" alt="..." />
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">${product.productName}</h5>
+          <p class="card-text">${product.price}</p>
+          <p class="card-text"></p>
+          <p>${product.description}</p>
+          <p class="card-text">${product.manufacturer}</p>
+          <p class="card-text">${product.quantity}</p>
+          <p class="card-text">${product.category.catName}</p>
+          <button type="button" class="btn btn-outline-dark">-</button>
+          <button type="button" class="btn btn-outline-dark">+</button>
+    
+          <button type="button" class="btn btn-outline-danger">Remove</button>
+        </div>
+      </div>
+    </div>
+    
+  </div> `;
+    productList.appendChild(row);
+  }
+
+  const buttons = document.querySelectorAll(".cartBtn");
+  buttons.forEach((btn) => {
+    btn.onclick = () => {
+      let productId = event.target.dataset.productId;
+      let sessionKey = sessionid.userId;
+      console.log(sessionKey);
+      addTocart(productId, sessionKey);
+    };
+    // console.log(btn)
+  });
+}
+
+function emptyCart() {
+  let sessionid = JSON.parse(sessionStorage.getItem("customer")) || null;
+  console.log(sessionid);
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  fetch(
+    `http://localhost:8080/carts/emptyCart?cartId=${sessionid.userId}`,
+    requestOptions
+  )
+    // .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+      if (result.status == 201) {
+        const productList = document.getElementById("productAppend");
+        productList.innerHTML = null;
+      }
+    })
+    .catch((error) => console.log("error", error));
+}
+
+function placeOrder() {
+  let sessionid = JSON.parse(sessionStorage.getItem("customer")) || null;
+  console.log(sessionid);
+  var requestOptions = {
+    method: "POST",
+    redirect: "follow",
+  };
+
+  fetch(
+    `http://localhost:8080/orders?customerId=${sessionid.userId}`,
+    requestOptions
+  )
+    // .then(response => response.text())
+    .then((result) => {
+      console.log(result);
+      if (result.status == 200) {
+        const productList = document.getElementById("productAppend");
+        productList.innerHTML = null;
+      }
+    })
+    .catch((error) => console.log("error", error));
+}
+
+window.addEventListener("load", () => {
+  let sessionid = JSON.parse(sessionStorage.getItem("customer_details"));
+  console.log(sessionid);
+  if (sessionid.customerId != 0) addProduct(sessionid.customerId);
+
+  document.getElementById("empty").onclick = () => {
+    emptyCart();
+  };
+  document.getElementById("plcorder").onclick = () => {
+    console.log("place order");
+    placeOrder();
+  };
+});
--- Shop Karo Frontend/scripts/sample.js
+++ Shop Karo Frontend/scripts/sample.js
@@ -1,24 +1,24 @@
 function getProducts() {
-    let sessionid = JSON.parse(localStorage.getItem("admin")) || null;
-    console.log(sessionid.uuid);
-  
-    var requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-  
-    fetch(
-      `http://localhost:8080/admin/products?sessionKey=${sessionid.uuid}`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => {
-        const productList = document.getElementById("product-list");
-        productList.innerHTML = ""; // Clear the product list before repopulating it
-        for (let product of result) {
-          const row = document.createElement("tr");
-  
-          row.innerHTML = `
+  let sessionid = JSON.parse(localStorage.getItem("admin")) || null;
+  console.log(sessionid.uuid);
+
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  fetch(
+    `http://localhost:8080/admin/products?sessionKey=${sessionid.uuid}`,
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      const productList = document.getElementById("product-list");
+      productList.innerHTML = ""; // Clear the product list before repopulating it
+      for (let product of result) {
+        const row = document.createElement("tr");
+
+        row.innerHTML = `
             <td>${product.productId}</td>
             <td>${product.productName}</td>
             <td><img src="${product.image}" alt="${product.productName}" width="100"></td>
@@ -29,41 +29,68 @@ function getProducts() {
             <td>${product.category.catName}</td>
             <td><button class="deleteProduct" data-product-id="${product.productId}" data-session-key="${sessionid.uuid}">Delete</button></td>
           `;
-          productList.appendChild(row);
-        }
-  
-        // Add event listeners to delete buttons
-        const deleteButtons = document.querySelectorAll(".deleteProduct");
-        deleteButtons.forEach((button) => {
-          button.addEventListener("click", (event) => {
-            const productId = event.target.dataset.productId;
-            const sessionKey = event.target.dataset.sessionKey;
-            deleteProduct(productId, sessionKey);
-          });
+        productList.appendChild(row);
+      }
+
+      // Add event listeners to delete buttons
+      const deleteButtons = document.querySelectorAll(".deleteProduct");
+      deleteButtons.forEach((button) => {
+        button.addEventListener("click", (event) => {
+          const productId = event.target.dataset.productId;
+          const sessionKey = event.target.dataset.sessionKey;
+          deleteProduct(productId, sessionKey);
         });
-      })
-      .catch((error) => console.log("error", error));
-  }
-  
-  function deleteProduct(productId, sessionKey) {
-    const requestOptions = {
-      method: "DELETE",
-      redirect: "follow",
-    };
-  
-    fetch(`http://localhost:8080/admin/products/${productId}?sessionKey=${sessionKey}`, requestOptions)
-      .then((response) => {
-        if (response.ok) {
-          // Remove the product from the DOM
-          const productRow = document.querySelector(`[data-product-id="${productId}"]`).parentNode.parentNode;
-          productRow.parentNode.removeChild(productRow);
-        } else {
-          throw new Error("Failed to delete product");
-        }
-      })
-      .catch((error) => console.log("error", error));
-  }
-  
-  document.getElementById("viewProducts").onclick = () => {
-    getProducts();
+      });
+    })
+    .catch((error) => console.log("error", error));
+}
+
+function deleteProduct(productId, sessionKey) {
+  const requestOptions = {
+    method: "DELETE",
+    redirect: "follow",
   };
+
+  fetch(
+    `http://localhost:8080/admin/products/${productId}?sessionKey=${sessionKey}`,
+    requestOptions
+  )
+    .then((response) => {
+      if (response.ok) {
+        // Remove the product from the DOM
+        const productRow = document.querySelector(
+          `[data-product-id="${productId}"]`
+        ).parentNode.parentNode;
+        productRow.parentNode.removeChild(productRow);
+      } else {
+        throw new Error("Failed to delete product");
+      }
+    })
+    .catch((error) => console.log("error", error));
+}
+
+document.getElementById("viewProducts").onclick = () => {
+  getProducts();
+};
+
+const id = "123"; // path variable as string
+const value = "456"; // request parameter as string
+
+fetch(`http://localhost:8080/carts/addProduct?cartId=${parseInt(id)}&productId=${parseInt(value)}`, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    // you can include a request body here, if needed
+  }),
+})
+  .then((response) => response.json())
+  .then((data) => {
+    // do something with the response data
+    console.log(data);
+  })
+  .catch((error) => {
+    // handle errors
+    console.error(error);
+  });
